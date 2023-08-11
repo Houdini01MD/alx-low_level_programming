7d8f385753a9611b904db83ca9d0b45c1cd95364
Houdini <ayoubelattr@gmail.com>
2023-08-11 10:51:22 -0700

    add 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unitstd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a C program that prints exactly followed by a new line
+ *
+ * Return: Always 1 (Not Success)
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
