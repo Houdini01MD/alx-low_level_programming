2a2f0a238047cdceb77cacbf3a6a132b3ca18225
Houdini <ayoubelattr@gmail.com>
2023-08-12 08:39:47 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints all single digit numbers of base 10
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i == 0 ; i < 10 ; i++)
+	{
+		printf(i);
+	}
+	printf("\n");
+	return (0);
+}
