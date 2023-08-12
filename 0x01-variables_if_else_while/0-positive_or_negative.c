d1cdf6825fc500c86bab666a319f9493d2daae9e
Houdini <ayoubelattr@gmail.com>
2023-08-12 06:08:39 -0700

    add first file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("is positive");
+		printf("%i is positive\n",n);
 	}
 	else if (n == 0)
 	{
-		printf("is zero");
+		printf("%i is zero\n",n);
 	}
 	else
 	{
-		printf("is negative");
+		printf("%i is negative\n",n);
 	}
 	return (0);
 }
