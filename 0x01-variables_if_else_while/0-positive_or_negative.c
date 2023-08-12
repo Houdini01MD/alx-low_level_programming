30b00c0ab214a510b9a47823f5261de2ab7b9cc1
Houdini <ayoubelattr@gmail.com>
2023-08-12 05:13:17 -0700

    add 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Enty point
+ *
+ * Description :  the number stored in the variable n is positive or negative.
+ *
+ * Return : Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n>0){
+		puts("is positive");
+	}else if(n=0){
+		puts("is zero");
+	}else{
+		puts("is negative");
+	}
+	return (0);
+}
