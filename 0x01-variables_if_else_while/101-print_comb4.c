2ca5f5aee7acac24cb38ce7fbd7cd2bd7d6afe14
Houdini <ayoubelattr@gmail.com>
2023-08-13 07:06:36 -0700

    add 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible different combinations of three digits
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+	int z;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			for (z = 2; j <= 9; z++)
+			{
+				if (i != j && j != z && i < j && j < z)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(z + '0');
+					if (i  j != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
