1761aa34d0b6318f30c6ae24f520db5960f50463
Houdini <ayoubelattr@gmail.com>
2023-08-16 11:54:58 -0700

    aqs

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ *
+ * @x: first number
+ * @y: second number
+ *
+ * Return: sum of x + y
+*/
+
+int add(int x, int y)
+{
+	int some;
+
+	some = x + y;
+	return (some);
+}
