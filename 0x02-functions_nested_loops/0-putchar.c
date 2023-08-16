010020673758d78bffe2f994afd5dbee6d1e22bd
Houdini <ayoubelattr@gmail.com>
2023-08-16 04:51:47 -0700

    first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints _putchar
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	puts("_putchar");
+	return ();
+}
