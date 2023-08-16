6ad246dd6327ef7055d36cd81e240763af8fb864
Houdini <ayoubelattr@gmail.com>
2023-08-16 11:18:13 -0700

    as

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  function that prints every minute of the day of Jack Bauer
+ *
+ * Return: 0 (success)
+*/
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
