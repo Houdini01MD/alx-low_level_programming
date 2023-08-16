bcd0523f118b534280003f05765858d2108c30f4
Houdini <ayoubelattr@gmail.com>
2023-08-16 08:41:50 -0700

    ml

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absolute value of an integer
+ *
+ * @n: the number to be checked
+ *
+ * Return: 0 (success)
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = n * (-1);
+		return (n);
+}
