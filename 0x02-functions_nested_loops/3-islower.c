c72792792a83c90532c80381806a205c2b90e614
Houdini <ayoubelattr@gmail.com>
2023-08-16 07:02:28 -0700

    add3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - function that checks for lowercase character
+ *
+ * @c: Returns 1 if c is lowercase Returns 0 otherwise
+*/
+
+int _islower(int c)
+{
+	if (c.islower())
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
