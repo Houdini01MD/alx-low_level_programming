e443720148dd3013a95b192a739c10748c3f8e63
Houdini <ayoubelattr@gmail.com>
2023-08-16 08:51:47 -0700

    ft

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints the last digit of a number
+ *
+ * @n: the number to be checked
+ *
+ * Return: 0 (success)
+*/
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
