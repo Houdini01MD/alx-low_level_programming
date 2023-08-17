a3bc992cdc3eba1b71b8290c6d96a2c335c2c9aa
Houdini <ayoubelattr@gmail.com>
2023-08-17 09:25:36 -0700

    add 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers a & b
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiplies (a*b)
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
