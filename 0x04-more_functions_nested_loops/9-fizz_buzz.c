7979ff0ee7860f33abdb6d81980989b3a9a0a583
Houdini <ayoubelattr@gmail.com>
2023-08-17 14:07:43 -0700

    add 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Fizz-Buzz - program that prints the numbers from 1 to 100
+ *		But for multiples of three print Fizz instead of the number
+ *		and for the multiples of five print Buzz.
+ *		For numbers which are multiples of both three and five print FizzBuzz
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf(" Fizz");
+			continue;
+		}
+		else if (i % 5 == 0)
+		{
+			printf(" Buzz");
+			continue;
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+			continue;
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+}
