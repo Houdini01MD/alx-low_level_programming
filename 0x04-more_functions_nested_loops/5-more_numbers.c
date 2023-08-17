0fcff4305c8c254725ec845eee82d0a3902aa0e9
Houdini <ayoubelattr@gmail.com>
2023-08-17 10:34:59 -0700

    add 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 times the numbers, from 0 to 14
+ *
+ * Return: numbers 0-14 10 times
+*/
+
+void more_numbers(void)
+{
+	int i, j, x;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			x = j;
+
+			if (j > 9)
+			{
+				_putchar(1 + '0');
+				x = j % 10;
+			}
+			_putchar(x + '0');
+		}
+		_putchar('\n');
+	}
+}
