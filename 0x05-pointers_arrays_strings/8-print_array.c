6464aad86677f390a89d2e82ec47ccfdc9ae323c
Houdini <ayoubelattr@gmail.com>
2023-08-23 08:42:32 -0700

    add 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_array -  function that prints n elements of an array of integers.
+ *
+ * @n: the number of elements
+ * @a: array
+ *
+ * Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+	}
+	_putchar('\n');
+}
