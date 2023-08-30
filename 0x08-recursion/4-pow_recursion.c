afca1034ea4303e1ca236e30a8e8a67547cf4bc3
Houdini <ayoubelattr@gmail.com>
2023-08-30 09:58:42 -0700

    add file 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion -  function that returns the value
+ *			of x raised to the power of y.
+ *
+ * @x: variable type int
+ * @y: variable type int
+ *
+ * Return: returns the value of x raised to the power of y.
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
