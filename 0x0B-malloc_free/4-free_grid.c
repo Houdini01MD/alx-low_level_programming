b4b6c07462d04951b504ddb49e2acc584cb86128
Houdini <ayoubelattr@gmail.com>
2023-09-05 11:28:47 -0700

    add file 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * free_grid - function that frees a 2 dimensional grid
+ * @grid: variable type int
+ * @height: variable type int
+ * Return: void
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
