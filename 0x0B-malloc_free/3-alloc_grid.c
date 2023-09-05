1e713426b0669a556694dd3950b5be5e93c994ea
Houdini <ayoubelattr@gmail.com>
2023-09-05 10:38:34 -0700

    add file 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function that returns a pointer
+ *			to a 2 dimensional array of integers
+ * @width: width of array
+ * @height: height of array
+ * Return:  2 dimensional array of integers
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr, i, j;
+
+	ptr = malloc(sizeof(**ptr) * height);
+	if (width <= 0 || height <= 0 || ptr == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			ptr[i] = malloc(sizeof(**ptr) * width);
+			if (ptr[i] == 0)
+			{
+				while (i--)
+				{
+					free(ptr[i]);
+				}
+				free(ptr);
+				return (NULL);
+			}
+			for (j = 0; j < width; j++)
+			{
+				ptr[i][j] = 0;
+			}
+		}
+	}
+	return (ptr);
+}
