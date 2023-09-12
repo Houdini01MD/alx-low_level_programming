f502206d417e6ca0d46898a38243d76c3a976a50
Houdini <ayoubelattr@gmail.com>
2023-09-12 09:16:57 -0700

    add file 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d: variable type dog
+ * Return: void
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s", d->owner);
+	}
+}
