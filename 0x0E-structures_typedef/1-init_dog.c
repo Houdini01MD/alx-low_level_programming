f2c59156b8d8bff273650ba42b4a3dd76c966e54
Houdini <ayoubelattr@gmail.com>
2023-09-12 07:29:30 -0700

    add file 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog -  function that initialize a variable of type struct dog
+ * @d:variable type dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ * Return: void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
