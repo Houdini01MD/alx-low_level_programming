3c5de5c8bd69b066d0162e08a319e8b63f259a78
Houdini <ayoubelattr@gmail.com>
2023-09-13 09:38:21 -0700

    add file 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer.
+ * @array: array to search in
+ * @size: size of array
+ * @cmp: pointer to the function to be used to compare values
+ * Return: int
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int s;
+
+	if (array && cmp)
+	{
+		for (s = 0; s < size; s++)
+		{
+			if (cmp(array[s] != 0)
+					return (i);
+		}
+	}
+	return (-1);
+}
