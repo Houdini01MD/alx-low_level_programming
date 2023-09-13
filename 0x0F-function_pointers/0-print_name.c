5a69239c7933d29783411d4d456030efdc1d4019
Houdini <ayoubelattr@gmail.com>
2023-09-13 07:26:31 -0700

    add file 0-print_name.C

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ * @name: name to print
+ * @f: pointer to function
+ * Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+	{
+		return;
+	}
+	else
+	{
+		f(name);
+	}
+}
