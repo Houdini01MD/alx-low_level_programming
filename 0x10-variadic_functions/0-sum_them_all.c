161ebf921ff7ad660970db331558a0de020b5d61
Houdini <ayoubelattr@gmail.com>
2023-09-14 23:54:27 -0700

    fix file 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,13 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list ptr;
+	int sum = 0;
+	unsigned int i;
+
 	if (n != 0)
 	{
-		va_list ptr;
-
 		va_start(ptr, n);
-		int i;
-		int sum = 0;
-
 		for (i = 0; i < n; i++)
 		{
 			sum += va_arg(ptr, int);
