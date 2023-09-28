ea9cd2ba14bae9934a9905e8f6b00fec050eb1c5
Houdini01MD <adjhoudini@gmail.com>
2023-09-28 16:29:24 -0700

    fix prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,6 +8,8 @@
 */
 int is_prime_number(int n)
 {
+	int i;
+
 	if (n <= 1)
 	{
 		return (0);
@@ -22,7 +24,7 @@ int is_prime_number(int n)
 	}
 	else
 	{
-		for (int i = 5; i * i <= n; i += 6)
+		for (i = 5; i * i <= n; i += 6)
 		{
 			if (n % i == 0 || n % (i + 2) == 0)
 			{
