6c7b46e45b0bc5495ebfff203c79aa5426cb3ca1
Houdini01MD <adjhoudini@gmail.com>
2023-09-28 15:02:05 -0700

    add factorial file

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - function that returns the factorial of a given number.
+ * @n: the number given from user
+ * Return: int
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
