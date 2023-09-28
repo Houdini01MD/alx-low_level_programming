4818f3e6302c94d83591430eea362d18e8a03938
Houdini01MD <adjhoudini@gmail.com>
2023-09-28 16:26:27 -0700

    add prime function

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * @n: the number
+ * Return: int
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n <= 3)
+	{
+		return (1);
+	}
+	else if (n % 2 == 0 || n % 3 == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		for (int i = 5; i * i <= n; i += 6)
+		{
+			if (n % i == 0 || n % (i + 2) == 0)
+			{
+				return (0);
+			}
+		}
+		return (1);
+	}
+}
