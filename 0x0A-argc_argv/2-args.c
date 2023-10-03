75e611abb2a888b434737ade2b5007e095858628
Houdini01MD <adjhoudini@gmail.com>
2023-10-03 10:40:17 +0000

    add task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that print the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
