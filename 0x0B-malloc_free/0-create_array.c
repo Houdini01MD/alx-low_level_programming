358f0239e05a5cf5a0dd6b0a29634dcefc368105
Houdini01MD <adjhoudini@gmail.com>
2023-10-04 18:28:58 +0000

    add main.h and _putchar.c and task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_array - function that creates an array of chars,
+ *                  and initializes it with a specific char.
+ * @size: size of array
+ * @c: variable type char
+ * Return: Returns a pointer to the array, or NULL if it fails
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+	free(ptr);
+
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+ this directory for 0x0B-malloc_free task 4/10/2023
