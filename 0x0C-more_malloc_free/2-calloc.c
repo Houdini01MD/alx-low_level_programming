0fff7f4c23451758cebe42b4f39227e9694e8a22
Houdini01MD <adjhoudini@gmail.com>
2023-10-09 11:29:38 +0000

    add function for task 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,10 +11,22 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr = malloc(nmemb * size);
+	void *ptr;
+	char *char_ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	char_ptr = (char *)ptr;
+
+	for (i = 0; i < nmemb * size; i++)
+		char_ptr[i] = 0;
 
 	return (ptr);
 
