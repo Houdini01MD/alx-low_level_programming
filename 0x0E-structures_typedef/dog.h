61cbe9ef79d8f616d37429e58d67dbcf70f0cbea
Houdini01MD <adjhoudini@gmail.com>
2023-10-11 12:07:11 +0000

    fix dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
 * struct dog - Define a new type struct dog
 */
@@ -13,6 +16,11 @@ typedef struct dog
 	char *owner;
 
 }dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
 
