46fffe2df2b8cf551d44ad0a6d6c66b384537375
Houdini01MD <adjhoudini@gmail.com>
2023-10-11 12:08:23 +0000

    fix dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,8 +5,14 @@
 #include <stdlib.h>
 
 /**
-* struct dog - Define a new type struct dog
-*/
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: Defines metadata about the dog and type of dog's struct
+ */
+
 typedef struct dog
 {
 	char *name;
@@ -15,7 +21,7 @@ typedef struct dog
 
 	char *owner;
 
-}dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
