e0e6ecff8302b2595159acd9859d76bf5f65fce2
Houdini01MD <adjhoudini@gmail.com>
2023-10-25 00:39:16 +0100

    add file 2 for task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,6 +13,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 
+	if (newNode == NULL)
+		return (NULL);
+
 	newNode->n = n;
 	newNode->next = *head;
 	*head = newNode;
