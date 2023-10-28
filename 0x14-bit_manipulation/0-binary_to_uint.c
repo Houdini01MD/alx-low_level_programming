4820a1fd4e2289d188265ddcc7128eedda09b3b7
Houdini01MD <adjhoudini@gmail.com>
2023-10-28 13:14:03 +0100

    create repo and add readme file and hearder and function binary_to_unit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: the converted number
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		dec = (dec << 1) | (*b - '0');
+
+		b++;
+	}
+	return (dec);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+this repo for bit manipulation tasks
