cc65b15b9d0c184ffa6606d211b65959817f5095
Houdini01MD <adjhoudini@gmail.com>
2023-10-31 14:32:21 +0100

    create main.c main.h 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+* read_textfile - reads a text file and prints.
+*
+* @filename: name of file
+* @letters: size of letters
+*
+* Return: returns the actual number of letters it could read and print
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file;
+
+	char *buffer;
+
+	ssize_t readB, writeB;
+
+	if (filename == NULL)
+		return (0);
+
+	file = fopen(filename, "r");
+	if (file == NULL)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
+
+	readB = fread(buffer, 1, letters, file);
+	if (readB == 0)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
+
+	writeB = fwrite(buffer, 1, readB, stdin);
+	if (writeB != readB)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
+
+	fclose(file);
+	free(buffer);
+	return (writeB);
+}
