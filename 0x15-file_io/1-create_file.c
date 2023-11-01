a667c48b39292ffaac0847eae450ff33ee719c65
Houdini01MD <adjhoudini@gmail.com>
2023-11-01 10:37:52 +0100

    update function create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, "rw");
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (file == -1)
 		return (-1);
 
