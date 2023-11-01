655a8305a8ea858e7075bf7dc8b5160847fd7c96
Houdini01MD <adjhoudini@gmail.com>
2023-11-01 11:47:15 +0100

    update cp function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,15 +1,53 @@
 #include "main.h"
 
 /**
- * exit_error - print error and exit
- * @error_code: error code
- * @message: message of error
- * Return: message
+* copy_file - print error and exit
+* @src: var type char
+* @dest: var type char
+* Return: void
 */
-void exit_error(int error_code, const char *message)
+void copy_file(const char *src, const char *dest)
 {
-    perror(message);
-    exit(error_code);
+	int fd_in;
+	int fd_out;
+	char buffer[4096];
+	ssize_t bytes_read;
+
+	fd_in = open(src, O_RDONLY);
+	if (fd_in == -1)
+	{
+		perror("Error opening source file");
+		exit(1);
+	}
+
+	fd_out = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+
+	if (fd_out == -1)
+	{
+		perror("Error opening destination file");
+		close(fd_in);
+		exit(1);
+	}
+
+	while ((bytes_read = read(fd_in, buffer, sizeof(buffer))) > 0)
+	{
+		if (write(fd_out, buffer, bytes_read) != bytes_read)
+		{
+			perror("Error writing to destination file");
+			close(fd_in);
+			close(fd_out);
+			exit(1);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		perror("Error reading from source file");
+		close(fd_in);
+		close(fd_out);
+		exit(1);
+	}
+	close(fd_in);
+	close(fd_out);
 }
 
 /**
@@ -20,63 +58,13 @@ void exit_error(int error_code, const char *message)
 */
 int main(int argc, char *argv[])
 {
-    const char *file_from;
-    const char *file_to;
-    int fd_from;
-    int fd_to;
-    char buffer[1024];
-    ssize_t bytes_read;
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s source destination\n", argv[0]);
+		exit(1);
+	}
 
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
+	copy_file(argv[1], argv[2]);
 
-    file_from = argv[1];
-    file_to = argv[2];
-
-    fd_from = open(file_from, O_RDONLY);
-    if (fd_from == -1)
-    {
-        exit_error(98, "Error: Can't read from file");
-    }
-
-    fd_to = open(file_to, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-    if (fd_to == -1)
-    {
-        close(fd_from);
-        exit_error(99, "Error: Can't write to file");
-    }
-
-    while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
-    {
-        ssize_t bytes_written = write(fd_to, buffer, bytes_read);
-        if (bytes_written != bytes_read)
-        {
-            close(fd_from);
-            close(fd_to);
-            exit_error(99, "Error: Can't write to file");
-        }
-    }
-
-    if (bytes_read == -1)
-    {
-        close(fd_from);
-        close(fd_to);
-        exit_error(98, "Error: Can't read from file");
-    }
-
-    if (close(fd_from) == -1)
-    {
-        close(fd_to);
-        exit_error(100, "Error: Can't close fd");
-    }
-
-    if (close(fd_to) == -1)
-    {
-        exit_error(100, "Error: Can't close fd");
-    }
-
-    return (0);
+	return (0);
 }
